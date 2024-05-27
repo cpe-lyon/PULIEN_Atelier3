e8c36c860fc9c12522b2a370fbc4bf8a1c525ea1
Maxime Brossard <maxime.brossard5@gmail.com>
2024-05-27 15:18:39 +0200

    feat: add service registry to microservice

--- micro-service-auth/src/main/java/org/pulien/cardmanager/CardManagerApplication.java
+++ micro-service-auth/src/main/java/org/pulien/cardmanager/CardManagerApplication.java
@@ -1,13 +1,14 @@
 package org.pulien.cardmanager;
 
-import jakarta.servlet.Filter;
-import org.pulien.cardmanager.authentification.JwtFilter;
 import org.pulien.cardmanager.authentification.JwtUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CardManagerApplication {
 
 	public static void main(String[] args) {
@@ -19,5 +20,3 @@ public class CardManagerApplication {
 		return new JwtUtil();
 	}
 }
-
-
