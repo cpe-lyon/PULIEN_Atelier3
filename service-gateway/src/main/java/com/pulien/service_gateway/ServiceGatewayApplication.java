009ed34c774a37f8b59a55e5f3755b89625ab627
Maxime Brossard <maxime.brossard5@gmail.com>
2024-05-28 08:08:54 +0200

    fix: pom.xml + start gateway

---
+++ service-gateway/src/main/java/com/pulien/service_gateway/ServiceGatewayApplication.java
@@ -0,0 +1,27 @@
+package com.pulien.service_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceGatewayApplication {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route("auth-manager",r -> r.path("/auth-manager/**")
+				.filters(f -> f.stripPrefix(1))
+				.uri("lb://AUTH-MANAGER"))
+				.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ service-gateway/src/test/java/com/pulien/service_gateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulien.service_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
